f1e37cbcea080d2d881c7c9db7039cbdcaa3ceda
Damar Albaribin <damaralbaribin@gmail.com>
2022-05-21 14:59:21 +0700

    fix : name

--- pages/sikomoku.tsx
+++ pages/sikomoku.tsx
@@ -99,7 +99,7 @@ const Sikomoku = () => {
         </Box>
         <Box component="div" className={classes.container}>
           <Typography color="secondary" variant="h4" className={classes.title}>
-            Dewi Barotuttaqiyah Taqiyah
+           Dewi Barotut Taqiyah
           </Typography>
           <Typography variant="h5" className={classes.subTitle}>
             NIM: 21129251031
